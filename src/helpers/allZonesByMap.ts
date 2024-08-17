19426860ba7832521d2ff406dcb24c2e1ffa6ac8
Guillaume ARM <guillaume.arm@payfit.com>
2024-08-18 01:13:03 +0200

    feat: add new places/zones/maps (streets + groundzero)

--- src/helpers/allZonesByMap.ts
+++ src/helpers/allZonesByMap.ts
@@ -26,6 +26,15 @@ export const allZonesByMap: Record<string, string[]> = {
     'place_skier_11_2',
     'huntsman_005_1',
     'huntsman_005_2',
+    'meh_45_radio_area_mark_1',
+    'meh_45_radio_area_mark_2',
+    'meh_45_radio_area_mark_3',
+    'meh_45_radio_area_mark_4',
+    'q14_8_1',
+    'q14_8_2',
+    'q14_8_3',
+    'q14_8_4',
+    'q14_8_5',
   ],
 
   shoreline: [
@@ -64,6 +73,9 @@ export const allZonesByMap: Record<string, string[]> = {
     'place_merch_21_3',
     'place_merch_020_1',
     'place_merch_020_2',
+    'quest_zone_keeper6_kiba_hide',
+    'quest_zone_keeper6_safe_hide',
+    'quest_zone_keeper6_cont_hide',
   ],
 
   factory: ['place_SADOVOD_01_1', 'place_SADOVOD_01_2', 'case_extraction', 'ter_017_area_1'],
@@ -79,6 +91,12 @@ export const allZonesByMap: Record<string, string[]> = {
     'qlight_fuel_blood_bezovoz2',
     'qlight_fuel_blood_bezovoz3',
     'qlight16_peace_terra',
+    'meh_42_radio_area_mark_1',
+    'meh_42_radio_area_mark_2',
+    'meh_42_radio_area_mark_3',
+    'meh_42_radio_area_mark_4',
+    'qlight_extension_medic1_hide1',
+    'qlight_extension_mechanik1_hide1',
   ],
 
   rezervbase: [
@@ -89,5 +107,33 @@ export const allZonesByMap: Record<string, string[]> = {
     'tadeush_stryker_area_mark_3',
     'baraholshik_fuel_area_2',
     'baraholshik_fuel_area_3',
+  ],
+
+  tarkovstreets: [
+    'quest_zone_place_c14_revx_1',
+    'quest_zone_place_c14_revx_2',
+    'quest_zone_place_c14_revx_3',
+    'quest_zone_keeper10_place',
+    'quest_zone_hide_sillent',
+    'quest_zone_hide_sillent2',
+    'quest_zone_hide_barber1',
+    'quest_zone_hide_barber2',
+    'quest_city_trotil2',
+    'quest_city_trotil1',
+  ],
+
+  groundzero: [],
+
+  // TODO: define on which map those should be
+  any: [
+    'quest_zone_place_c24_tigr1',
+    'quest_zone_place_c24_tigr2',
+    'tadeush_bmp2_area_mark_12',
+    'place_keeper5_1',
+    'place_keeper5_2',
+    'quest_zone_keeper8_1_hide1',
+    'quest_zone_keeper8_1_hide2',
+    'quest_zone_hide_2st_mech',
+    'place_flyers1',
   ],
 };
